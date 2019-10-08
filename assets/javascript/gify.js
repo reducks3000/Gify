381f090f2986609b4fd25f0a21c2a9435057b131
Reducks <reducks3000@gmail.com>
2019-10-08 02:28:04 -0500

    added variables and giphy ajax call and basic function

---
+++ assets/javascript/gify.js
@@ -0,0 +1,23 @@
+//gify animal array
+let animals = ["Cat", "Dog", "Horse", "Seal"];
+
+function displayGif() {
+    let animals = $(this).attr("data-name");
+    let queryURL = "https://api.giphy.com/v1/gifs/search?q=" + animals + "&api_key=WHqpLXdFmw6xzWwMkiuMJUZdLjE6NGBR";
+
+//ajax call
+$.ajax({url: queryURL, method: "GET"}).done(function(response){
+//empties out gif div
+    $('#gif').empty();
+
+    for (let i = 0; i < response.data.length; i++) {
+        let gifDiv = $('<div class = "gifDiv">');
+        let rating = response.data[i].rating;
+        let ratingDiv = $('<p>').html("Rating: " + rating);
+        let animated = response.data[i].images.fixed_height.url;
+        let still = response.data[i].images.fixed_height_still.url;
+        let gifImg = $('<img class="gifImg">');
+    }
+
+});
+};
